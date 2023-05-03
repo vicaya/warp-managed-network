0de5208d21d403a2ed66834c173e0cd5822d9d9c
Luke Lu <luke.lu@petuum.com>
2023-05-03 04:53:50 +0000

    Log user agent if present

--- warp-local-network-id.py
+++ warp-local-network-id.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import http.server
+import logging
 import ssl
 import sys
 
@@ -13,11 +14,12 @@ class HelloHandler(http.server.BaseHTTPRequestHandler):
     self.send_header('Content-type', 'text/plain; charset=utf-8')
     self.end_headers()
     self.wfile.write(f'{self.hello}'.encode('utf-8'))
+    self.log_message("%s", self.headers.get("user-agent", "unknown ua"))
 
 
-def serve(prefix, hello):
+def serve(port, prefix, hello):
   HelloHandler.hello = hello
-  server = http.server.HTTPServer(('0.0.0.0', 4443), HelloHandler)
+  server = http.server.ThreadingHTTPServer(('0.0.0.0', port), HelloHandler)
   sslcontext = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
   sslcontext.load_cert_chain(certfile=f'{prefix}.pem', keyfile=f'{prefix}.key')
   server.socket = sslcontext.wrap_socket(server.socket, server_side=True)
@@ -26,8 +28,9 @@ def serve(prefix, hello):
 
 def main(args):
   nargs = len(args)
-  serve(args[1] if nargs > 1 else "default",
-        args[2] if nargs > 2 else "hello!")
+  serve(int(args[1]) if nargs > 1 else 4443,
+        args[2] if nargs > 2 else "default",
+        args[3] if nargs > 3 else "hello!\n")
 
 
 if "__main__" == __name__:
